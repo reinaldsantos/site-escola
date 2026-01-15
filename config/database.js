a5de806051f735b5370cbe6d7ecbc7c80dab4539
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-15 17:10:29 +0000

    Enable SSL for PostgreSQL connection

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
