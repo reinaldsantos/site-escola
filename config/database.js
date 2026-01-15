eb395984bd64859bc3fdeec14392bb24fc9edda8
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-15 16:43:13 +0000

    Change database from SQLite to PostgreSQL for Render deployment

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 ﻿module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi_5cj5'),
+      user: env('DATABASE_USERNAME', 'strapidbsantos___p9ft_user'),
+      password: env('DATABASE_PASSWORD', 'RE6QZLoFUQHvWV0iZAKIr6D5zf8wAMqG'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    useNullAsDefault: true,
+    pool: {
+      min: env.int('DATABASE_POOL_MIN', 0),
+      max: env.int('DATABASE_POOL_MAX', 10),
+    },
   },
 });
