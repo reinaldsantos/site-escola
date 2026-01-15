8c05a90f1bedde832465dbb067c294c2d46b49bc
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-15 16:53:57 +0000

    Final PostgreSQL migration - database.js updated for Render

--- config/database.js
+++ config/database.js
@@ -1,17 +1,15 @@
-﻿module.exports = ({ env }) => ({
+﻿'use strict';
+
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_5cj5'),
-      user: env('DATABASE_USERNAME', 'strapidbsantos___p9ft_user'),
-      password: env('DATABASE_PASSWORD', 'RE6QZLoFUQHvWV0iZAKIr6D5zf8wAMqG'),
-      ssl: env.bool('DATABASE_SSL', true),
-    },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 0),
-      max: env.int('DATABASE_POOL_MAX', 10),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
