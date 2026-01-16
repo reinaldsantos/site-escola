965aadbe0eb8ef55c217e675719e99fc86901ec2
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-16 10:38:55 +0000

    FIX: PostgreSQL SSL config for Render

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 ﻿'use strict';
 
-// CONFIGURAÇÃO OTIMIZADA PARA RENDER
+// CONFIGURAÇÃO SUPER SIMPLES PARA RENDER
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false
-      }
+      host: env('DATABASE_HOST', 'dpg-d5kgas94tr6s73au58pg-a.ohio-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi_5cj5'),
+      user: env('DATABASE_USERNAME', 'strapidbsantos___p9ft_user'),
+      password: env('DATABASE_PASSWORD', 'RE6QZLoFUQHvWV0iZAKIr6D5zf8wAMqG'),
+      ssl: { rejectUnauthorized: false }
     },
-    debug: false,
   },
 });
