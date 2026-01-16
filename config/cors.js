8ede54ffdc77dee53ee9a373e108f87cb1557da3
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-16 13:46:54 +0000

    FIX: Configuração CORS correta para Strapi 4+

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,7 @@
 ﻿module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      origin: env("CORS_ALLOW_ORIGIN", "*"),
-      enabled: true,
-    },
-  },
+  enabled: true,
+  origin: env.array('CORS_ORIGIN', ['*']),
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+  keepHeaderOnError: true,
 });
