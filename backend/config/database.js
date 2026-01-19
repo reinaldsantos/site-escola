200712a880d29931e67e67911c6e5721ff35462d
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-19 17:27:13 +0000

    FIX: Correct database.js configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,16 @@
 ﻿module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
