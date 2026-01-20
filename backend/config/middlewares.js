74666af58f46514253dd7046432cbb831d3315de
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-20 15:11:25 +0000

    FIX CORS: Allow access from Vercel production site

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,7 +1,32 @@
 ﻿module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://site-escola-five-sand.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:3001'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -10,3 +35,4 @@
   'strapi::favicon',
   'strapi::public',
 ];
+
